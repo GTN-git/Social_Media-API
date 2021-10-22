7806f85832fc4f2c13d63a076866014f4d8d4148
George Ngo <georgengo@msn.com>
2021-10-22 15:51:33 -0700

    controller and models set up

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model, Types } = require("mongoose")
+const dateFormat = require("../utils/dateFormat")
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.\..+/],
+    },
+
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+)
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length
+})
+
+const User = model("User", UserSchema)
+
+module.exports = User
